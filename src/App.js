a02b9b4a7b51d035b8dcdddd91b04a43c04c17fe
abknowledge63@gmail.com <abknowledge63@gmail.com>
2021-05-23 00:32:32 -0400

    second feature push

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Golody Keep on deploying on ElasticBeanStack using on feature branch
+          Hi Golody Keep on deploying on ElasticBeanStack using on feature branch 3.0
         </p>
         <a
           className="App-link"
