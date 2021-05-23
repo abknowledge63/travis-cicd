d0e1498a63e81b6d799f7772f33df5869d870701
abknowledge63@gmail.com <abknowledge63@gmail.com>
2021-05-23 00:13:01 -0400

    added changed after feature branch update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Golody Keep on understanding again then again on ElasticBeanStack with Dedication
+          Hi Golody Keep on deploying on ElasticBeanStack using on feature branch
         </p>
         <a
           className="App-link"
